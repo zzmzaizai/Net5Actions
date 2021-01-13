fd946b5e3d0837cfcf74dc054b60481df59eb4c1
痞子再 <zzmzaizai@msn.com>
2021-01-13 21:19:11 +0800

    完成简单demo

---
+++ src/SpiderTask.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpiderTask.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! args:{0}", string.Join(',', args));
+        }
+
+
+         
+
+
+
+
+    }
+}
