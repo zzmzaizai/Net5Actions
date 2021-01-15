133a46fba8bdb8e9f76aff189e33fa96d8ce0f7a
痞子再 <zzmzaizai@msn.com>
2021-01-15 21:45:13 +0800

    ceshi3

--- src/SpiderTask.ConsoleApp/Program.cs
+++ src/SpiderTask.ConsoleApp/Program.cs
@@ -17,23 +17,7 @@ namespace SpiderTask.ConsoleApp
 
 
 
-            //依次打印三种级别的环境变量
-
-            Console.WriteLine("计算机的环境变量");
-            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine))
-            {
-                Console.WriteLine("{0}:{1}", item.Key, item.Value);
-            }
-            Console.WriteLine();
-
-
-            Console.WriteLine("用户的环境变量");
-
-            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User))
-            {
-                Console.WriteLine("{0}:{1}", item.Key, item.Value);
-            }
-            Console.WriteLine();
+ 
 
             Console.WriteLine("进程的环境变量");
 
@@ -41,21 +25,7 @@ namespace SpiderTask.ConsoleApp
             {
                 Console.WriteLine("{0}:{1}", item.Key, item.Value);
             }
-
-            //一次性打印所有三种环境变量（直接利用枚举器的遍历
-
-            var targets = Enum.GetNames(typeof(EnvironmentVariableTarget));
-            foreach (var target in targets)
-            {
-                Console.WriteLine("{0}级别的环境变量", target);
-                EnvironmentVariableTarget t = (EnvironmentVariableTarget)Enum.Parse(typeof(EnvironmentVariableTarget), target);
-
-                foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(t))
-                {
-                    Console.WriteLine("{0}:{1}", item.Key, item.Value);
-                }
-
-            }
+             
 
         }
 
