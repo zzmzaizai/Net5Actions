d02686c2d8758d8ce3d5a732289fd8ecb42ccadf
痞子再 <zzmzaizai@msn.com>
2021-01-15 21:36:01 +0800

    ceshi1

--- src/SpiderTask.ConsoleApp/Program.cs
+++ src/SpiderTask.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace SpiderTask.ConsoleApp
 {
@@ -13,10 +14,53 @@ namespace SpiderTask.ConsoleApp
             }
             Console.WriteLine("Hello World! args:{0} {1}", cs, cs.Length);
             //Console.WriteLine("Hello World! args:{0} {1}", string.Join(',', args).ToCharArray().,args.Length);
+
+
+
+            //依次打印三种级别的环境变量
+
+            Console.WriteLine("计算机的环境变量");
+            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine))
+            {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("用户的环境变量");
+
+            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User))
+            {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("进程的环境变量");
+
+            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables())
+            {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+
+            //一次性打印所有三种环境变量（直接利用枚举器的遍历
+
+            var targets = Enum.GetNames(typeof(EnvironmentVariableTarget));
+            foreach (var target in targets)
+            {
+                Console.WriteLine("{0}级别的环境变量", target);
+                EnvironmentVariableTarget t = (EnvironmentVariableTarget)Enum.Parse(typeof(EnvironmentVariableTarget), target);
+
+                foreach (DictionaryEntry item in Environment.GetEnvironmentVariables(t))
+                {
+                    Console.WriteLine("{0}:{1}", item.Key, item.Value);
+                }
+
+            }
+
         }
 
 
-         
+
 
 
 
