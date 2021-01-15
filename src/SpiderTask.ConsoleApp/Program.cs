027f4ea3bb3de169cb8af022ed16d6989c6dea14
痞子再 <zzmzaizai@msn.com>
2021-01-15 20:34:06 +0800

    改代码验证下想法

--- src/SpiderTask.ConsoleApp/Program.cs
+++ src/SpiderTask.ConsoleApp/Program.cs
@@ -6,7 +6,13 @@ namespace SpiderTask.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! args:{0} {1}", string.Join(',', args),args.Length);
+            var cs = "";
+            foreach(var c in string.Join('-', args).ToCharArray())
+            {
+                cs = c + cs;
+            }
+            Console.WriteLine("Hello World! args:{0} {1}", cs, args.Length);
+            //Console.WriteLine("Hello World! args:{0} {1}", string.Join(',', args).ToCharArray().,args.Length);
         }
 
 
